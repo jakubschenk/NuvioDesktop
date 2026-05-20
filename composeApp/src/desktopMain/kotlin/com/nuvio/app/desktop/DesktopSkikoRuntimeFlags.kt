f7d0ea8f0feb82b1c6feee88c63658fb5cc6aca1
Jakub Schenk <jakubschenk3@gmail.com>
2026-05-20 13:12:56 +0200

    Use component layers for ANGLE player overlay

--- composeApp/src/desktopMain/kotlin/com/nuvio/app/desktop/DesktopSkikoRuntimeFlags.kt
+++ composeApp/src/desktopMain/kotlin/com/nuvio/app/desktop/DesktopSkikoRuntimeFlags.kt
@@ -31,7 +31,7 @@ internal object DesktopSkikoRuntimeFlags {
         val composeLayersType = firstEnv("NUVIO_COMPOSE_LAYERS_TYPE", "COMPOSE_LAYERS_TYPE")
             ?.normalizeComposeLayersType()
             ?: System.getProperty("compose.layers.type")?.normalizeComposeLayersType()
-            ?: if (mpvSurface == "native-window") "WINDOW" else null
+            ?: if (mpvSurface == "native-window") "COMPONENT" else null
         if (composeLayersType != null) {
             setProperty("compose.layers.type", composeLayersType, applied)
         }
